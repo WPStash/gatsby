e326670485a7c8e43d2dab094ec9551ef4373458
Kien T <kienpc81089@gmail.com>
2016-08-02 13:11:35 +0700

    Test deploy: Change footer info style + text

--- footer.php
+++ footer.php
@@ -28,14 +28,14 @@
 			<div class="container">
 				<?php
 				if ( has_nav_menu( 'footer' ) ) {
-					wp_nav_menu( array( 'theme_location' => 'footer' ) ); 
+					wp_nav_menu( array( 'theme_location' => 'footer' ) );
 				}
 				?>
 
 				<div class="site-copyright">
 					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gatsby' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'gatsby' ), 'WordPress' ); ?></a>
 					<span class="sep"> . </span>
-					<?php printf( esc_html__( 'Theme by %2$s.', 'gatsby' ), 'gatsby', '<a href="https://wpstash.com" rel="designer">WPStash</a>' ); ?>
+					<?php printf( esc_html__( 'Theme designed by %2$s.', 'gatsby' ), 'gatsby', '<a href="https://wpstash.com" rel="designer">WPStash</a>' ); ?>
 				</div>
 			</div>
 		</div><!-- .site-info -->
